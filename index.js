80af62f52fb27e96f69a5d63224984c057ece5d3
w01fgang <sumin@unix-center.ru>
2017-11-14 17:03:22 +0100

    🎉 Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const emojis = require('./emojis');
+
+let regxArr = [];
+const noExtra = s => s.replace(/\ufe0f|\u200d/gm, '');
+const toSurrogatePairs = (r) => {
+  let t;
+  let n;
+  /* eslint-disable no-plusplus */
+  for (t = '', n = 0; n < r.length; n++) {
+    t += `\\u${`000${r[n].charCodeAt(0).toString(16)}`.substr(-4)}`;
+  }
+  return t;
+};
+const toCodePoint = (t) => {
+  let n;
+  let r;
+  let o;
+  let h;
+  for (n = [], r = 0, o = 0, h = 0; h < t.length;) {
+    (r = t.charCodeAt(h++)),
+    o
+      ? (n.push((65536 + ((o - 55296) << 10) + (r - 56320)).toString(16)), (o = 0))
+      : r >= 55296 && r <= 56319 ? (o = r) : n.push(r.toString(16));
+  }
+  return n.join('-');
+};
+for (let i = 0; i < emojis.length; i++) {
+  regxArr.push(toSurrogatePairs(emojis[i]));
+}
+const re = new RegExp(`(${regxArr.join('|')})`, 'g');
+regxArr = null;
+
+class Parser {
+  constructor(path) {
+    if (!path) {
+      throw new Error('You should pass images path as the parameter to the constructor');
+    }
+    this.path = path;
+  }
+
+  parse(string) {
+    return string.replace(
+      re,
+      (a, b) => `<img draggable="false" src="${this.path + toCodePoint(noExtra(b))}.png">`,
+    );
+  }
+}
+
+module.exports = Parser;
---
+++ index.test.js
@@ -0,0 +1,15 @@
+const Parser = require('.');
+
+describe('Emoji parser', () => {
+  it('should save path parameter', () => {
+    const path = '/images/';
+    const parser = new Parser(path);
+    expect(parser.path).toBe(path);
+  });
+
+  it('should parse emoji', () => {
+    const parser = new Parser('/images/');
+    const result = parser.parse('Text 🙂');
+    expect(result).toBe('Text <img draggable="false" src="/images/1f642.png">');
+  });
+});
