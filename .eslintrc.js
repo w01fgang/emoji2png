716f07b525b7825d75e6d2af60f74917a3220036
w01fgang <sumin@unix-center.ru>
2017-11-16 13:09:27 +0100

    :bug: remove traling comma for functions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-    "extends": "airbnb-base"
+  extends: 'airbnb-base',
+  env: { jest: true, browser: true },
+  rules: {
+    'function-paren-newline': 0,
+  },
 };
--- index.js
+++ index.js
@@ -42,8 +42,7 @@ class Parser {
     return string.replace(
       re,
       (a, b) =>
-        `<img draggable="false" class="emoji" src="${this.path + toCodePoint(noExtra(b))}.png">`,
-    );
+        `<img draggable="false" class="emoji" src="${this.path + toCodePoint(noExtra(b))}.png">`);
   }
 }
 
